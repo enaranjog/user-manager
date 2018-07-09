b3b05d8136f045e81f14a737284e5fe0dbc2e062
admin <admin@admin.com>
2018-07-09 17:56:46 +0200

    allowing CORS

--- src/main/java/com/enrique/starter/UserManagerStarter.java
+++ src/main/java/com/enrique/starter/UserManagerStarter.java
@@ -3,22 +3,24 @@ package com.enrique.starter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import com.enrique.controller.UserManagementController;
-import com.enrique.managers.GameManager;
-import com.enrique.managers.PlayerManager;
-import com.enrique.managers.UserManager;
-import com.enrique.repositories.GameRepository;
-import com.enrique.repositories.PlayerRepository;
-import com.enrique.repositories.UserRepository;
-import com.enrique.security.CheckUserAndPasswordService;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaRepositories(basePackages = { "com.enrique.repositories" })
 @SpringBootApplication(scanBasePackages={"com.enrique.security", "com.enrique.managers", "com.enrique.controller"})
 @EntityScan(basePackages = { "com.enrique.entities" })
-public class UserManagerStarter {
+public class UserManagerStarter implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins("*")
+		.allowedMethods("*")
+		.allowedHeaders("*")
+		.allowCredentials(false)
+		.maxAge(4800);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagerStarter.class, args);
